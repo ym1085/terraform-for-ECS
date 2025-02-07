ebef4d3021de710c8b1c5d66ef832717e286ce24
ymkim <ymkim1085@funin.camp>
2025-02-07 12:26:33 +0900

    fix: s3 backend storage 주석 처리
    1. backend의 경우 s3 버킷이 생성 된 이후에 연동이 필요하기에 우선 주석 처리

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -16,12 +16,12 @@
   - !important
     - dynamodb_table은 미리 생성되어 있어야함
 */
-terraform {
-  backend "s3" {
-    bucket         = "terraform-s3-ymkim-state" # s3 버킷명
-    key            = "dev/terraform.tfstate"    # state 파일 s3 저장 경로
-    region         = "ap-northeast-2"           # s3 리전
-    encrypt        = true                       # terraform.state 파일 암호화 지정 여부
-    dynamodb_table = "tfstate-lock"             # DynamoDB 테이블명, DynamoDB 테이블을 사용한 잠금 기능(동시성 제어)
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-s3-ymkim-state" # s3 버킷명
+#     key            = "dev/terraform.tfstate"    # state 파일 s3 저장 경로
+#     region         = "ap-northeast-2"           # s3 리전
+#     encrypt        = true                       # terraform.state 파일 암호화 지정 여부
+#     dynamodb_table = "tfstate-lock"             # DynamoDB 테이블명, DynamoDB 테이블을 사용한 잠금 기능(동시성 제어)
+#   }
+# }
